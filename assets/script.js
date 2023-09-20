23e1fe29a04055b8d7dcfdc066e026295c80b739
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2023-09-19 20:30:41 -0400

    added javascript to project

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var i =0; i < topics.length; i++){
+    console.log("Let's Study " + topics[i] + "!");
+}
+
+/*
+switch(topic) {
+    case 'HTML':
+        console.log(repeatedPhrase + topic + "!");
+        break;
+    case 'CSS':
+        console.log(repeatedPhrase + topic + "!");
+        break;
+    case 'Git':
+        console.log(repeatedPhrase + topic + "!");
+        break;
+    case 'JavaScript':
+        console.log(repeatedPhrase + topic + "!");
+        break;
+    default:
+        console.log('Please Try Again!')
+
+}
+*/
