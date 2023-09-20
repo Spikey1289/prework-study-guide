793c3aa6f969dd29fa7d2d12bcb2e405c0298d5d
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2023-09-20 16:57:01 -0400

    added Functions to the Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,42 @@
+function listTopics(inputPhraseX) {    // function using a for loop to print out the topics in an input array
+    for (var i =0; i < inputPhraseX.length; i++){
+        console.log(inputPhraseX[i]);
+    }
+}
+
+function selectTopic(){    //function using a switching statement to determine a console output based on an input
+    switch(randomTopic) {
+        case 'HTML':
+            console.log(repeatedPhrase + randomTopic + "!");
+            break;
+        case 'CSS':
+            console.log(repeatedPhrase + randomTopic + "!");
+            break;
+        case 'Git':
+            console.log(repeatedPhrase + randomTopic + "!");
+            break;
+        case 'JavaScript':
+            console.log(repeatedPhrase + randomTopic + "!");
+            break;
+        default:
+            console.log('Please Try Again!');
+    
+    }
+}
+
+//declarations
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var repeatedPhrase = "Let's Study ";
 
-for (var i =0; i < topics.length; i++){
-    console.log("Let's Study " + topics[i] + "!");
-}
 
-/*
-switch(topic) {
-    case 'HTML':
-        console.log(repeatedPhrase + topic + "!");
-        break;
-    case 'CSS':
-        console.log(repeatedPhrase + topic + "!");
-        break;
-    case 'Git':
-        console.log(repeatedPhrase + topic + "!");
-        break;
-    case 'JavaScript':
-        console.log(repeatedPhrase + topic + "!");
-        break;
-    default:
-        console.log('Please Try Again!')
+//randomly choosing a topic
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
 
-}
-*/
+
+console.log('Here are the topics we learned through Prework:');
+
+//calling functions
+listTopics(topics);
+
+console.log('Which topic should we study first?');
+selectTopic();
